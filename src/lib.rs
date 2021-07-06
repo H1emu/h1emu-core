9bb5b1d50077dcddd8e151be7f14a3a50506cf64
Quentin Gruber <quentingruber@gmail.com>
2021-07-06 14:19:02 +0200

    try to opti

--- src/lib.rs
+++ src/lib.rs
@@ -9,14 +9,14 @@ pub fn generate_random_guid() -> String {
     rand_id.push_str("0x");
     for byte in array_bytes {
         let formatted_byte: String = format!("{:X}", byte);
-        if formatted_byte.len() < 2 {
+        if formatted_byte.len() == 1 {
             let mut formatted_byte_with_additionnal_zero: String = "0".to_string();
             formatted_byte_with_additionnal_zero.push_str(&formatted_byte);
             rand_id.push_str(&formatted_byte_with_additionnal_zero);
         } else {
             rand_id.push_str(&formatted_byte);
         }
-        if rand_id.len() == 16 {
+        if rand_id.len() == 18 {
             break;
         }
     }
