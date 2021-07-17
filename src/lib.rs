deea4d8bc0da37acf61ca364bba8acce078a23df
Quentin Gruber <quentingruber@gmail.com>
2021-07-18 01:24:52 +0200

    gen random guid in lower case ( 0.2.1 )

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ pub fn generate_random_guid() -> String {
 
     const ASCII_ZERO: u8 = '0' as u8;
     const ASCII_NINE: u8 = '9' as u8;
-    const ASCII_NUMBERS_LETTERS_OFFSET: u8 = 'A' as u8 - '9' as u8 - 1;
+    const ASCII_NUMBERS_LETTERS_OFFSET: u8 = 'a' as u8 - '9' as u8 - 1;
 
     for i in 0..8 {
         let mut leading = random[i] / 16 + ASCII_ZERO;
@@ -29,6 +29,7 @@ mod tests {
     use super::*;
     #[test]
     fn generate_random_guid_test() {
+        println!( "{:?}" , generate_random_guid() );
         assert_ne!(generate_random_guid(), generate_random_guid())
     }
 }
