d6167294168a26a18a10667069fcfde23620101f
Quentin Gruber <quentingruber@gmail.com>
2021-09-09 00:43:30 +0200

    reorganize src + add joaat

---
+++ src/jenkins.rs
@@ -0,0 +1,24 @@
+// https://en.wikipedia.org/wiki/Jenkins_hash_function#one_at_a_time
+pub fn joaat(string: &str) -> u32{
+    let mut hash:u32 = 0;
+    for &i in string.as_bytes() {
+        hash += i as u32;
+        hash += hash << 10;
+        hash ^= hash >> 6;
+    }
+    hash += hash << 3;
+    hash ^= hash >> 11;
+    hash += hash << 15;
+    return hash >> 0;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn jenkins_oaat_test() {
+        println!( "{:?}" , joaat("HAX") );
+        assert_eq!(joaat("HAX"), 2813495259)
+    }
+}
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,4 @@
-use wasm_bindgen::prelude::*;
-use rand::random;
-#[wasm_bindgen]
-pub fn generate_random_guid() -> String {
-    let random: [u8; 8] = random();
-
-    let mut str_bytes = vec![0u8; 16];
-
-    const ASCII_ZERO: u8 = '0' as u8;
-    const ASCII_NINE: u8 = '9' as u8;
-    const ASCII_NUMBERS_LETTERS_OFFSET: u8 = 'a' as u8 - '9' as u8 - 1;
-
-    for i in 0..8 {
-        let mut leading = random[i] / 16 + ASCII_ZERO;
-        let mut trailing = random[i] % 16 + ASCII_ZERO;
-
-        leading += ((leading > ASCII_NINE) as u8) * ASCII_NUMBERS_LETTERS_OFFSET;
-        trailing += ((trailing > ASCII_NINE) as u8) * ASCII_NUMBERS_LETTERS_OFFSET;
-
-        str_bytes[2 * i] = leading;
-        str_bytes[2 * i + 1] = trailing;
-    }
-
-    unsafe { String::from_utf8_unchecked(str_bytes) }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn generate_random_guid_test() {
-        println!( "{:?}" , generate_random_guid() );
-        assert_ne!(generate_random_guid(), generate_random_guid())
-    }
-}
+mod utils;
+mod jenkins;
+pub use utils::*;
+pub use jenkins::*;
