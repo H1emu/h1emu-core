fdd1b3850f63a60af52ac4e99020715fca7a19d5
Quentin Gruber <quentingruber@gmail.com>
2021-09-23 22:40:30 +0200

    add append_crc() & session_reply()

--- src/crc.rs
+++ src/crc.rs
@@ -1,6 +1,18 @@
 mod crc_table;
 use crc_table::get_crc_table;
 use wasm_bindgen::prelude::*;
+use byteorder::{BigEndian,WriteBytesExt};
+
+
+#[wasm_bindgen]
+pub fn append_crc(data: &[u8], crc_seed: usize) -> std::vec::Vec<u8>{
+  let mut data_mut = data.to_vec();
+  let crc = crc32(&data_mut, crc_seed >> 0);
+  let mut crc_buffer =  vec![];
+  crc_buffer.write_u16::<BigEndian>((crc & 0xffff) as u16).unwrap();
+  data_mut.extend(crc_buffer);
+  return data_mut;
+}
 
 #[wasm_bindgen]
 pub fn crc32(data: &[u8], crc_seed: usize) -> u32{
@@ -32,6 +44,11 @@ mod tests {
     fn crc32_test() {
         let data:[u8;5] =  [0, 21, 0, 0, 2];
         assert_eq!(crc32(&data, 0), 1874907695)
+    }
+    #[test]
+    fn append_crc_test(){
+      let data:[u8;24] = [0, 9, 0, 0, 0, 169, 183, 185, 67, 241, 64, 164, 5, 143, 19, 35, 87, 21, 163, 205, 26, 83, 24, 212];
+      assert_eq!(append_crc(&data, 0), [0, 9, 0, 0, 0, 169, 183, 185, 67, 241, 64, 164, 5, 143, 19, 35, 87, 21, 163, 205, 26, 83, 24, 212, 220, 81])
     }
 }
 
