401433769dabce737588f6e53584898ffb76a011
Quentin Gruber <quentingruber@gmail.com>
2022-03-17 03:21:43 +0100

    rmv old code

--- src/soeprotocol.rs
+++ src/soeprotocol.rs
@@ -55,26 +55,6 @@ impl Soeprotocol {
     
 }
 
-
-#[wasm_bindgen]
-pub fn session_reply(
-    session_id: u32,
-    crc_seed: u32,
-    crc_length: u8,
-    compression: u16,
-    udp_length: u32,
-) -> std::vec::Vec<u8> {
-    let mut wtr = vec![];
-    wtr.write_u16::<BigEndian>(0x02).unwrap();
-    wtr.write_u32::<BigEndian>(session_id).unwrap();
-    wtr.write_u32::<BigEndian>(crc_seed).unwrap();
-    wtr.write_u8(crc_length).unwrap();
-    wtr.write_u16::<BigEndian>(compression).unwrap();
-    wtr.write_u32::<BigEndian>(udp_length).unwrap();
-    wtr.write_u32::<BigEndian>(3).unwrap();
-    return wtr;
-}
-
 #[cfg(test)]
 mod tests {
 
