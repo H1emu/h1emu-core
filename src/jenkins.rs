c11a16e1edfe79e042c95784518f952b32d8f58a
Quentin Gruber <quentingruber@gmail.com>
2022-05-25 09:26:54 +0200

    fix potential overflow error for joaat and add wikipedia tests

--- src/jenkins.rs
+++ src/jenkins.rs
@@ -4,13 +4,13 @@ use wasm_bindgen::prelude::*;
 pub fn joaat(string: &str) -> u32 {
     let mut hash: u32 = 0;
     for &i in string.as_bytes() {
-        hash += i as u32;
-        hash += hash << 10;
+        hash = hash.wrapping_add(i as u32);
+        hash = hash.wrapping_add(hash << 10);
         hash ^= hash >> 6;
     }
-    hash += hash << 3;
+    hash = hash.wrapping_add(hash << 3);
     hash ^= hash >> 11;
-    hash += hash << 15;
+    hash = hash.wrapping_add(hash <<15);
     return hash >> 0;
 }
 
@@ -20,5 +20,10 @@ mod tests {
     #[test]
     fn jenkins_oaat_test() {
         assert_eq!(joaat("HAX"), 2813495259)
+    }
+    #[test]
+    fn jenkins_oaat_wikipedia_tests() {
+        assert_eq!(joaat("The quick brown fox jumps over the lazy dog"), 0x519e91f5);
+        assert_eq!(joaat("a"), 0xca2e9442)
     }
 }
