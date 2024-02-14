a84cd540f724bd45ae2b23678098856255a55e4c
quentingruber <quentingruber@gmail.com>
2024-02-14 11:55:56 +0100

    refactored gatewayprotocol

--- src/gatewayprotocol.rs
+++ src/gatewayprotocol.rs
@@ -7,7 +7,7 @@ use super::gatewayprotocol_packets_structs::*;
 use super::lib_utils::read_prefixed_string_le;
 
 #[wasm_bindgen]
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
 pub enum GatewayOpcode {
     LoginRequest = 0x01,
     LoginReply = 0x02,
@@ -17,6 +17,7 @@ pub enum GatewayOpcode {
     TunnelDataServer = 0x06,
     ChannelIsRoutable = 0x07,
     ChannelIsNotRoutable = 0x08,
+    Unknown = 0x00,
 }
 #[wasm_bindgen]
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -29,165 +30,75 @@ pub enum GatewayChannels {
 }
 
 #[wasm_bindgen]
-pub struct GatewayProtocol {
-    wtr: Vec<u8>,
-}
+pub struct GatewayProtocol {}
 
 #[wasm_bindgen]
 impl GatewayProtocol {
     #[wasm_bindgen(constructor)]
     pub fn initialize() -> GatewayProtocol {
-        GatewayProtocol { wtr: vec![] }
+        GatewayProtocol {}
     }
-    pub fn parse(&mut self, data: Vec<u8>) -> String {
-        let mut rdr = Cursor::new(&data);
-        if data.len() < 2 {
-            return format!(r#"{{"name":"Unknown","raw":{:?}}}"#, data);
+    pub fn get_opcode(&mut self, opcode: u8) -> GatewayOpcode {
+        match opcode {
+            0x01 => GatewayOpcode::LoginRequest,
+            0x02 => GatewayOpcode::LoginReply,
+            0x03 => GatewayOpcode::Logout,
+            0x04 => GatewayOpcode::ForceDisconnect,
+            0x05 => GatewayOpcode::TunnelDataClient,
+            0x06 => GatewayOpcode::TunnelDataServer,
+            0x07 => GatewayOpcode::ChannelIsRoutable,
+            0x08 => GatewayOpcode::ChannelIsNotRoutable,
+            _ => GatewayOpcode::Unknown,
         }
+    }
+    pub fn parse(&mut self, data: Vec<u8>) -> GatewayPacketParsed {
+        let mut rdr = Cursor::new(&data);
+        // if data.len() < 2 {
+        //     return format!(r#"{{"name":"Unknown","raw":{:?}}}"#, data);
+        // }
         let full_opcode = rdr.read_u8().unwrap_or_default();
         let opcode = full_opcode & 0x1f;
-        let channel = full_opcode >> 5;
+        // let channel = full_opcode >> 5;
 
-        match opcode {
-            0x01 => self.parse_login_request(rdr),
-            0x02 => self.parse_login_reply(rdr),
-            0x03 => r#"{"name":"Logout"}"#.to_string(),
-            0x04 => r#"{"name":"ForceDisconnect"}"#.to_string(),
-            0x05 => self.parse_tunnel_data(data),
-            0x06 => self.parse_tunnel_data(data),
-            0x07 => self.parse_channel_is_routable(rdr),
-            0x08 => self.parse_channel_is_not_routable(rdr),
-            _ => format!(
-                r#"{{"name":"Unknown","channel":{},"raw":{:?}}}"#,
-                channel, data
+        let gateway_opcode = self.get_opcode(opcode);
+
+        match gateway_opcode {
+            GatewayOpcode::Unknown => GatewayPacketParsed::new(
+                GatewayOpcode::Unknown,
+                GatewayPacket::Unknown(UnknownGatewayPacket {}),
+            ),
+            GatewayOpcode::LoginRequest => GatewayPacketParsed::new(
+                GatewayOpcode::LoginRequest,
+                GatewayPacket::LoginRequest(LoginRequestPacket::from(rdr)),
+            ),
+            GatewayOpcode::LoginReply => GatewayPacketParsed::new(
+                GatewayOpcode::LoginReply,
+                GatewayPacket::LoginReply(LoginReplyPacket::from(rdr)),
+            ),
+            GatewayOpcode::Logout => GatewayPacketParsed::new(
+                GatewayOpcode::Logout,
+                GatewayPacket::Logout(LogoutPacket::from(rdr)),
+            ),
+            GatewayOpcode::ForceDisconnect => GatewayPacketParsed::new(
+                GatewayOpcode::ForceDisconnect,
+                GatewayPacket::ForceDisconnect(ForceDisconnectPacket::from(rdr)),
+            ),
+            GatewayOpcode::TunnelDataClient => GatewayPacketParsed::new(
+                GatewayOpcode::TunnelDataClient,
+                GatewayPacket::Tunnel(TunnelPacket::from(data)),
+            ),
+            GatewayOpcode::TunnelDataServer => GatewayPacketParsed::new(
+                GatewayOpcode::TunnelDataServer,
+                GatewayPacket::Tunnel(TunnelPacket::from(data)),
+            ),
+            GatewayOpcode::ChannelIsRoutable => GatewayPacketParsed::new(
+                GatewayOpcode::ChannelIsRoutable,
+                GatewayPacket::ChannelIsRoutable(ChannelIsRoutablePacket::from(rdr)),
+            ),
+            GatewayOpcode::ChannelIsNotRoutable => GatewayPacketParsed::new(
+                GatewayOpcode::ChannelIsNotRoutable,
+                GatewayPacket::ChannelIsNotRoutable(ChannelIsNotRoutablePacket::from(rdr)),
             ),
         }
-    }
-
-    pub fn pack_login_request_packet(
-        &mut self,
-        character_id: u64,
-        ticket: String,
-        client_protocol: String,
-        client_build: String,
-    ) -> Vec<u8> {
-        self.pack_login_request_object(LoginRequestPacket::new(
-            character_id,
-            ticket,
-            client_protocol,
-            client_build,
-        ))
-    }
-    pub fn pack_login_reply_packet(&mut self, logged_in: bool) -> Vec<u8> {
-        self.pack_login_reply_object(LoginReplyPacket { logged_in })
-    }
-    pub fn pack_tunnel_data_packet_for_client(&mut self, data: Vec<u8>, channel: u8) -> Vec<u8> {
-        self._pack_tunnel_data_packet(0x05, data, channel)
-    }
-    pub fn pack_tunnel_data_packet_for_server(&mut self, data: Vec<u8>, channel: u8) -> Vec<u8> {
-        self._pack_tunnel_data_packet(0x06, data, channel)
-    }
-    fn _pack_tunnel_data_packet(
-        &mut self,
-        base_opcode: u8,
-        mut data: Vec<u8>,
-        channel: u8,
-    ) -> Vec<u8> {
-        let opcode = base_opcode | channel << 5;
-        self.wtr.clear();
-        self.wtr.write_u8(opcode).unwrap_or_default();
-        self.wtr.append(&mut data);
-        self.wtr.clone()
-    }
-    pub fn pack_channel_is_routable_packet(&mut self) -> Vec<u8> {
-        let opcode = 0x07;
-        self.wtr.clear();
-        self.wtr.write_u8(opcode).unwrap_or_default();
-        self.wtr.clone()
-    }
-    pub fn pack_channel_is_not_routable_packet(&mut self) -> Vec<u8> {
-        let opcode = 0x08;
-        self.wtr.clear();
-        self.wtr.write_u8(opcode).unwrap_or_default();
-        self.wtr.clone()
-    }
-}
-
-impl GatewayProtocol {
-    fn parse_login_request(&mut self, mut rdr: Cursor<&std::vec::Vec<u8>>) -> String {
-        let character_id = rdr.read_u64::<LittleEndian>().unwrap_or_default();
-        let raw_data = rdr.clone().into_inner();
-        let ticket_data_pos = rdr.position();
-        let ticket_data_len = rdr.read_u32::<LittleEndian>().unwrap_or_default();
-        let ticket = read_prefixed_string_le(raw_data, ticket_data_pos as usize, ticket_data_len);
-        rdr.set_position(ticket_data_pos + ticket_data_len as u64 + 4);
-        let client_protocol_data_pos = rdr.position();
-        let client_protocol_data_len = rdr.read_u32::<LittleEndian>().unwrap_or_default();
-        let client_protocol = read_prefixed_string_le(
-            raw_data,
-            client_protocol_data_pos as usize,
-            client_protocol_data_len,
-        );
-        rdr.set_position(client_protocol_data_pos + client_protocol_data_len as u64 + 4);
-        let client_build_data_pos = rdr.position();
-        let client_build_data_len = rdr.read_u32::<LittleEndian>().unwrap_or_default();
-        let client_build = read_prefixed_string_le(
-            raw_data,
-            client_build_data_pos as usize,
-            client_build_data_len,
-        );
-        format!(
-            r#"{{"name":"LoginRequest","character_id":"0x{:x}","ticket":"{}","client_protocol":"{}","client_build":"{}"}}"#,
-            character_id, ticket, client_protocol, client_build
-        )
-    }
-    fn parse_login_reply(&mut self, mut rdr: Cursor<&std::vec::Vec<u8>>) -> String {
-        let logged_in: bool = rdr.read_u8().unwrap_or_default() != 0; // convert to bool
-        format!(r#"{{"name":"LoginReply","logged_in":{}}}"#, logged_in)
-    }
-    fn parse_tunnel_data(&mut self, mut data: std::vec::Vec<u8>) -> String {
-        let channel = data.remove(0) >> 5;
-        let tunnel_data = data;
-        let packet = TunnelPacket::new(channel, tunnel_data);
-        serde_json::to_string(&packet).unwrap_or_default()
-    }
-    fn parse_channel_is_routable(&mut self, mut _rdr: Cursor<&std::vec::Vec<u8>>) -> String {
-        format!(r#"{{"name":"ChannelIsRoutable","raw":"{:?}"}}"#, _rdr)
-    }
-    fn parse_channel_is_not_routable(&mut self, mut _rdr: Cursor<&std::vec::Vec<u8>>) -> String {
-        format!(r#"{{"name":"ChannelIsNotRoutable","raw":"{:?}"}}"#, _rdr)
-    }
-
-    pub fn pack_login_request_object(&mut self, packet: LoginRequestPacket) -> Vec<u8> {
-        self.wtr.clear();
-        self.wtr.write_u8(0x01).unwrap_or_default();
-        self.wtr
-            .write_u64::<LittleEndian>(packet.character_id)
-            .unwrap_or_default();
-        let ticket = packet.get_ticket();
-        self.wtr
-            .write_u32::<LittleEndian>(ticket.len() as u32)
-            .unwrap_or_default();
-        self.wtr.append(&mut ticket.as_bytes().to_vec());
-        let client_protocol = packet.get_client_protocol();
-        self.wtr
-            .write_u32::<LittleEndian>(client_protocol.len() as u32)
-            .unwrap_or_default();
-        self.wtr.append(&mut client_protocol.as_bytes().to_vec());
-        let client_build = packet.get_client_build();
-        self.wtr
-            .write_u32::<LittleEndian>(client_build.len() as u32)
-            .unwrap_or_default();
-        self.wtr.append(&mut client_build.as_bytes().to_vec());
-        self.wtr.clone()
-    }
-
-    pub fn pack_login_reply_object(&mut self, packet: LoginReplyPacket) -> Vec<u8> {
-        self.wtr.clear();
-        self.wtr.write_u8(0x02).unwrap_or_default();
-        self.wtr
-            .write_u8(packet.logged_in as u8)
-            .unwrap_or_default();
-        self.wtr.clone()
     }
 }
--- src/gatewayprotocol_packets_structs.rs
+++ src/gatewayprotocol_packets_structs.rs
@@ -1,14 +1,22 @@
 use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
 
+use super::lib_utils::read_prefixed_string_le;
+
 use super::gatewayprotocol::GatewayOpcode;
+use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
+use std::io::Cursor;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum GatewayPacket {
     LoginRequest(LoginRequestPacket),
     LoginReply(LoginReplyPacket),
     ChannelIsRoutable(ChannelIsRoutablePacket),
+    ChannelIsNotRoutable(ChannelIsNotRoutablePacket),
     Tunnel(TunnelPacket),
+    Unknown(UnknownGatewayPacket),
+    Logout(LogoutPacket),
+    ForceDisconnect(ForceDisconnectPacket),
 }
 
 #[wasm_bindgen]
@@ -16,6 +24,68 @@ pub enum GatewayPacket {
 pub struct GatewayPacketParsed {
     opcode: GatewayOpcode,
     packet: GatewayPacket,
+}
+#[wasm_bindgen]
+impl GatewayPacketParsed {
+    pub fn get_opcode(&self) -> GatewayOpcode {
+        self.opcode
+    }
+    pub fn get_login_request_packet(&self) -> Option<LoginRequestPacket> {
+        match &self.packet {
+            GatewayPacket::LoginRequest(packet) => Some(packet.clone()),
+            _ => None,
+        }
+    }
+    pub fn get_login_reply_packet(&self) -> Option<LoginReplyPacket> {
+        match &self.packet {
+            GatewayPacket::LoginReply(packet) => Some(packet.clone()),
+            _ => None,
+        }
+    }
+    pub fn get_channel_is_routable_packet(&self) -> Option<ChannelIsRoutablePacket> {
+        match &self.packet {
+            GatewayPacket::ChannelIsRoutable(packet) => Some(packet.clone()),
+            _ => None,
+        }
+    }
+    pub fn get_channel_is_not_routable_packet(&self) -> Option<ChannelIsNotRoutablePacket> {
+        match &self.packet {
+            GatewayPacket::ChannelIsNotRoutable(packet) => Some(packet.clone()),
+            _ => None,
+        }
+    }
+    pub fn get_tunnel_packet(&self) -> Option<TunnelPacket> {
+        match &self.packet {
+            GatewayPacket::Tunnel(packet) => Some(packet.clone()),
+            _ => None,
+        }
+    }
+    pub fn get_unknown_packet(&self) -> Option<UnknownGatewayPacket> {
+        match &self.packet {
+            GatewayPacket::Unknown(packet) => Some(packet.clone()),
+            _ => None,
+        }
+    }
+    pub fn get_logout_packet(&self) -> Option<LogoutPacket> {
+        match &self.packet {
+            GatewayPacket::Logout(packet) => Some(packet.clone()),
+            _ => None,
+        }
+    }
+    pub fn get_force_disconnect_packet(&self) -> Option<ForceDisconnectPacket> {
+        match &self.packet {
+            GatewayPacket::ForceDisconnect(packet) => Some(packet.clone()),
+            _ => None,
+        }
+    }
+}
+impl GatewayPacketParsed {
+    pub fn new(opcode: GatewayOpcode, packet: GatewayPacket) -> Self {
+        Self { opcode, packet }
+    }
+    pub fn get_packet(&self) -> GatewayPacket {
+        self.packet.clone()
+    }
 }
 
 #[wasm_bindgen]
@@ -28,6 +98,7 @@ pub struct LoginRequestPacket {
 }
 #[wasm_bindgen]
 impl LoginRequestPacket {
+    #[wasm_bindgen(constructor)]
     pub fn new(
         character_id: u64,
         ticket: String,
@@ -53,6 +124,57 @@ impl LoginRequestPacket {
     pub fn get_client_build(&self) -> String {
         self.client_build.clone()
     }
+    pub fn build(&self) -> Vec<u8> {
+        let mut wtr: Vec<u8> = vec![];
+        wtr.write_u8(GatewayOpcode::LoginRequest as u8)
+            .unwrap_or_default();
+        wtr.write_u64::<LittleEndian>(self.character_id)
+            .unwrap_or_default();
+        let ticket = self.get_ticket();
+        wtr.write_u32::<LittleEndian>(ticket.len() as u32)
+            .unwrap_or_default();
+        wtr.append(&mut ticket.as_bytes().to_vec());
+        let client_protocol = self.get_client_protocol();
+        wtr.write_u32::<LittleEndian>(client_protocol.len() as u32)
+            .unwrap_or_default();
+        wtr.append(&mut client_protocol.as_bytes().to_vec());
+        let client_build = self.get_client_build();
+        wtr.write_u32::<LittleEndian>(client_build.len() as u32)
+            .unwrap_or_default();
+        wtr.append(&mut client_build.as_bytes().to_vec());
+        wtr
+    }
+}
+impl LoginRequestPacket {
+    pub fn from(mut rdr: Cursor<&std::vec::Vec<u8>>) -> LoginRequestPacket {
+        let character_id = rdr.read_u64::<LittleEndian>().unwrap_or_default();
+        let raw_data = rdr.clone().into_inner();
+        let ticket_data_pos = rdr.position();
+        let ticket_data_len = rdr.read_u32::<LittleEndian>().unwrap_or_default();
+        let ticket = read_prefixed_string_le(raw_data, ticket_data_pos as usize, ticket_data_len);
+        rdr.set_position(ticket_data_pos + ticket_data_len as u64 + 4);
+        let client_protocol_data_pos = rdr.position();
+        let client_protocol_data_len = rdr.read_u32::<LittleEndian>().unwrap_or_default();
+        let client_protocol = read_prefixed_string_le(
+            raw_data,
+            client_protocol_data_pos as usize,
+            client_protocol_data_len,
+        );
+        rdr.set_position(client_protocol_data_pos + client_protocol_data_len as u64 + 4);
+        let client_build_data_pos = rdr.position();
+        let client_build_data_len = rdr.read_u32::<LittleEndian>().unwrap_or_default();
+        let client_build = read_prefixed_string_le(
+            raw_data,
+            client_build_data_pos as usize,
+            client_build_data_len,
+        );
+        LoginRequestPacket {
+            character_id,
+            ticket,
+            client_protocol,
+            client_build,
+        }
+    }
 }
 #[wasm_bindgen]
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -61,11 +183,25 @@ pub struct LoginReplyPacket {
 }
 #[wasm_bindgen]
 impl LoginReplyPacket {
+    #[wasm_bindgen(constructor)]
     pub fn new(logged_in: bool) -> LoginReplyPacket {
         LoginReplyPacket { logged_in }
     }
     pub fn get_logged_in(&self) -> bool {
         self.logged_in
+    }
+    pub fn build(&self) -> Vec<u8> {
+        let mut wtr: Vec<u8> = vec![];
+        wtr.write_u8(GatewayOpcode::LoginReply as u8)
+            .unwrap_or_default();
+        wtr.write_u8(self.logged_in as u8).unwrap_or_default();
+        wtr
+    }
+}
+impl LoginReplyPacket {
+    pub fn from(mut rdr: Cursor<&std::vec::Vec<u8>>) -> LoginReplyPacket {
+        let logged_in: bool = rdr.read_u8().unwrap_or_default() != 0; // convert to bool
+        LoginReplyPacket { logged_in }
     }
 }
 
@@ -76,25 +212,41 @@ pub struct ChannelIsRoutablePacket {
 }
 #[wasm_bindgen]
 impl ChannelIsRoutablePacket {
+    #[wasm_bindgen(constructor)]
     pub fn new(is_routable: bool) -> ChannelIsRoutablePacket {
         ChannelIsRoutablePacket { is_routable }
     }
     pub fn get_is_routable(&self) -> bool {
         self.is_routable
     }
+    pub fn build(&self) -> Vec<u8> {
+        let mut wtr: Vec<u8> = vec![];
+        wtr.write_u8(GatewayOpcode::ChannelIsRoutable as u8)
+            .unwrap_or_default();
+        wtr
+    }
 }
 
+impl ChannelIsRoutablePacket {
+    pub fn from(mut rdr: Cursor<&std::vec::Vec<u8>>) -> ChannelIsRoutablePacket {
+        let is_routable = rdr.read_u8().unwrap_or_default() == 1;
+        ChannelIsRoutablePacket { is_routable }
+    }
+}
 #[wasm_bindgen]
 #[derive(Serialize, Deserialize, Debug, Clone)]
 // Internal
 pub struct TunnelPacket {
     pub channel: u8,
+    pub opcode: GatewayOpcode,
     tunnel_data: Vec<u8>,
 }
 #[wasm_bindgen]
 impl TunnelPacket {
-    pub fn new(channel: u8, tunnel_data: Vec<u8>) -> TunnelPacket {
+    #[wasm_bindgen(constructor)]
+    pub fn new(opcode: GatewayOpcode, channel: u8, tunnel_data: Vec<u8>) -> TunnelPacket {
         TunnelPacket {
+            opcode,
             channel,
             tunnel_data,
         }
@@ -104,5 +256,56 @@ impl TunnelPacket {
     }
     pub fn get_tunnel_data(&self) -> Vec<u8> {
         self.tunnel_data.clone()
+    }
+    pub fn build(&mut self) -> Vec<u8> {
+        let mut wtr: Vec<u8> = vec![];
+        wtr.write_u8(self.opcode as u8).unwrap_or_default();
+        wtr.append(&mut self.tunnel_data);
+        wtr
+    }
+}
+impl TunnelPacket {
+    pub fn from(mut data: std::vec::Vec<u8>) -> TunnelPacket {
+        let channel = data.remove(0) >> 5;
+        let tunnel_data = data;
+        // FIXME: opcode
+        TunnelPacket {
+            opcode: GatewayOpcode::TunnelDataServer,
+            channel,
+            tunnel_data,
+        }
+    }
+}
+
+#[wasm_bindgen]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct ChannelIsNotRoutablePacket {}
+impl ChannelIsNotRoutablePacket {
+    pub fn from(mut _rdr: Cursor<&std::vec::Vec<u8>>) -> ChannelIsNotRoutablePacket {
+        ChannelIsNotRoutablePacket {}
+    }
+}
+#[wasm_bindgen]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct LogoutPacket {}
+impl LogoutPacket {
+    pub fn from(mut _rdr: Cursor<&std::vec::Vec<u8>>) -> LogoutPacket {
+        LogoutPacket {}
+    }
+}
+#[wasm_bindgen]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct ForceDisconnectPacket {}
+impl ForceDisconnectPacket {
+    pub fn from(mut _rdr: Cursor<&std::vec::Vec<u8>>) -> ForceDisconnectPacket {
+        ForceDisconnectPacket {}
+    }
+}
+#[wasm_bindgen]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct UnknownGatewayPacket {}
+impl UnknownGatewayPacket {
+    pub fn from(mut _rdr: Cursor<&std::vec::Vec<u8>>) -> UnknownGatewayPacket {
+        UnknownGatewayPacket {}
     }
 }
